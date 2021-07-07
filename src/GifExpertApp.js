683692d59facec54f88cdf165781a9aaf0493daf
Samir Mejia <samirmejiaat@unimagdalena.edu.co>
2021-07-07 13:19:17 -0500

    Mostar lista de elementos

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GifExpertApp = () => {
+
+    // const categories = ['One Puch', 'Samurai X', 'Dragon Ball'];
+    const [categories, setCategories] = useState(['One Puch', 'Samurai X', 'Dragon Ball']);
+
+    const handleAdd = () => {
+        setCategories( [...categories, 'Akame ga kill'] );
+    }
+
     return (
         <>
             <h2>GifExpertApp</h2>
             <hr/>
+
+            <button onClick={ handleAdd }>Agregar</button>
+
+            <ol>
+                { 
+                    categories.map( category => {
+                        return <li key={ category } > { category } </li>
+                    })
+                }
+            </ol>
         </>
     )
 }
